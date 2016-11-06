375f0f196233173972ffaa0f9516191774090838
Wildan S. Nahar <wildansnahar@gmail.com>
2016-11-06 22:48:34 +0700

    update setup again

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-var express         = require('express')
-var app             = express()
-var bodyParser      = require('body-parser')
-var mongoose        = require('mongoose')
-var methodOverride  = require('method-override')
+var express         = require('express');
+var app             = express();
+var bodyParser      = require('body-parser');
+var mongoose        = require('mongoose');
+var methodOverride  = require('method-override');
 var port            = process.env.PORT || 4000;
 
 // routes
-var Temp    = require('./models/temp')
-var temps   = require('./routes/temps')
-var Humid   = require('./models/humid')
-var humids  = require('./routes/humids')
+var Temp    = require('./models/temp');
+var temps   = require('./routes/temps');
+var Humid   = require('./models/humid');
+var humids  = require('./routes/humids');
 
-mongoose.connect('mongodb://localhost:27017/api')
+mongoose.connect('mongodb://localhost:27017/api');
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(methodOverride('_method'))
-app.set('view engine', 'jade')
-app.set('views', './views')
-app.use('/api', temps)
-app.use('/api', humids)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine', 'jade');
+app.set('views', './views');
+app.use('/api', temps);
+app.use('/api', humids);
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Simple-API', message: 'Temps and Humidity'});
 });
 
-app.listen(port)
-console.log('listening on port', port)
+app.listen(port);
+console.log('listening on port', port);
