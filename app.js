16a627d08911361484374adee5816437d0c4eda5
Wildan S. Nahar <wildansnahar@gmail.com>
2016-11-06 23:07:38 +0700

    grant privileges

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var temps   = require('./routes/temps');
 var Humid   = require('./models/humid');
 var humids  = require('./routes/humids');
 
-mongoose.connect('mongodb://localhost:27017/api');
+mongoose.connect('mongodb://127.0.0.1:27017/api');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
